01eeab3d69aba93d3c98671ad593a3e238db66e5
arose42-jh <arose42@jh.edu>
2025-06-08 20:37:13 -0400

    made some preliminary pizza tests

---
+++ module_4/tests/pizza_test.py
@@ -0,0 +1,33 @@
+import pytest
+from pizzaorders.pizza import Pizza
+
+# @pytest.fixture
+# def nonepizza(): #left beef
+#     return Pizza()
+
+@pytest.fixture
+def basicpizza():
+    pizza = Pizza('thin', ['pesto'], 'Mozzarella', ['mushrooms'])
+    return pizza
+
+def what_is_a_pizza(basicpizza):
+    assert True if basicpizza else False
+    assert type(basicpizza[1]) == str
+    assert type(basicpizza[2]) == list
+    assert type(basicpizza[2][0]) == str
+    assert basicpizza[3] == 'Mozzarella'
+    assert type(basicpizza[4]) == list
+    assert basicpizza.cost > 0 
+
+def say_the_pizza(basicpizza):
+    pizzastring = str(basicpizza)
+    assert "thin" in pizzastring
+    assert 'pesto' in pizzastring
+    assert 'Mozzarella' in pizzastring
+    assert 'mushrooms' in pizzastring
+    assert '$11.00' in pizzastring
+
+#Make more pizzas for calculator
+def cost_calculator(basicpizza):
+    nocoupon = basicpizza.cost 
+    assert nocoupon == 11
